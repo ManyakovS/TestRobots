2ff0edd633f3d539d105bd88e04d5fd979317553
Rafael2111 <117172668+Rafael2111@users.noreply.github.com>
2023-06-20 21:25:41 +0700

    feat: Webkit кнопки, заголовки, компоненты: header, homePage

--- src/stores/RobotStore.ts
+++ src/stores/RobotStore.ts
@@ -1,11 +1,29 @@
 import { defineStore } from 'pinia';
 
-export const useRobotStore = defineStore("RobotStore", {
+export const useRobotStore = defineStore('RobotStore', {
 
     state: () => {
         return {
-            value: 'Тест',
-    }
-    }
+            /* Параметры текста */
+            /* header */
+            header_text_button: 'Произвести биоробота',
 
-})
+            /* home page */
+            homePage_text: 'Фабрика по производству биороботов',
+
+            /*  */
+
+
+
+            /* Монеты роботы */
+        }
+    },
+
+    actions: {
+        increment() {
+            this.count++
+        },
+
+    }
+}
+)
